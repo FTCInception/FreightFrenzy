e051c16e98ce568b3bfe10f5482088b3dbbff08c
taschelling <taschelling@gmail.com>
2021-11-06 19:26:02 -0500

    Fix enums for vision return value.

--- Red_DuckSide_Auto.java
+++ Red_DuckSide_Auto.java
@@ -69,7 +69,7 @@ public class Red_DuckSide_Auto extends LinearOpMode {
     private static boolean option3 = true;
 
     private IncepVision vision = new IncepVision();
-    private int grnLocation = vision.gUNSEEN;
+    private IncepVision.MarkerPos grnLocation = IncepVision.MarkerPos.Unseen;
     private Trajectory[][] trajs = {new Trajectory[25], new Trajectory[25], new Trajectory[25]} ;
 
     @Override
@@ -187,9 +187,9 @@ public class Red_DuckSide_Auto extends LinearOpMode {
         // TODO: Make sure the LEFT/RIGHT/UNSEEN mapping here is correct for every auto.
         // FIXME: Test the stopping in auto here again.
         if (opModeIsActive()) {
-            if (grnLocation == vision.gRIGHT) {
+            if (grnLocation == IncepVision.MarkerPos.Outer) {
                 Level1(trajs[LEVEL1]);
-            } else if (grnLocation == vision.gLEFT) {
+            } else if (grnLocation == IncepVision.MarkerPos.Inner) {
                 Level2(trajs[LEVEL2]);
             } else {
                 // If it's not LEFT or RIGHT, assume unseen
